031b697d59f5517ced578cccac1ded549efbb088
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-16 11:13:22 -0700

    completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - entry point
+ *
+ * Description: A C program prints with printf funtion
+ *
+ * Return: alwys 0 (success)
+*/
+
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
