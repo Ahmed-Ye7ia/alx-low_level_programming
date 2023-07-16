974128b4bb0d90c498887b015be2908f223724dd
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-16 07:48:35 -0700

    completed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  *
  * Description: A C program prints with printf funtion
  *
- * return: alwys 0 (success)
+ * Return: alwys 0 (success)
 */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
