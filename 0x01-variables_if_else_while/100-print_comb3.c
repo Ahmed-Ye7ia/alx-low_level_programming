1837ed0e9ad0bdcec38e9c958539b5e8cc321b67
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-17 13:30:11 -0700

    completed

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase and uppercase.
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int d1 = 0, d2 = 1;
+
+	while (d1 < 9)
+	{
+		while (d2 <= 9)
+		{
+			putchar(48 + d1);
+			putchar(48 + d2);
+			putchar(',');
+			putchar(' ');
+			d2++;
+		}
+		d1++;
+		d2++;
+	}
+	putchar('\n');
+	return (0);
+}
