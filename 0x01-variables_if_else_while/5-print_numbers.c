c8befefe767867d98e2ac05109432d4535b08616
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-17 11:47:15 -0700

    completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase.
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf(n);
+		n++;
+
+	}
+	putchar('\n');
+	return (0);
+}
