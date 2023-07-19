c79de187f203a39713c72e4ef7be0f90e59d0124
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-19 08:04:17 -0700

    completed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _islower - A C function that checks for alphabe
+ * _isalpha - A C function that checks for alphabe
  *
  * @c: char the will check on it
  *
  * Return: 1 if c is letter 0 otherwise
 */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
