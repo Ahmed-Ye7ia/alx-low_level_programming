9ec7b3ecc02568ae3c230a9fcdfe1b39a0041c25
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-19 07:45:35 -0700

    completed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,11 +10,11 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	else if (c >= 'A' && c <= 'Z')
+	else if (c >= 65 && c <= 90)
 	{
 		return (0);
 	}
