49cb89bd82ec3be8044384a252969fe207aadb71
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-21 07:56:38 -0700

    completed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,16 @@
 
 void print_line(int n)
 {
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		for (int i = 1 ; i <= n ; i++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
