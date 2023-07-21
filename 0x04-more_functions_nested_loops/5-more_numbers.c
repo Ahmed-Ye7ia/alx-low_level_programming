c5f7cf7535d8addc82217a9dc23517a0681ac002
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-21 06:51:39 -0700

    completed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,19 @@ void more_numbers(void)
 {
 	int i, num, count;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (count = 0 ; count < 14 ; count++)
+		for (count = 0; count < 14; count++)
 		{
 			num = count;
 			if (count > 9)
 			{
-				_putchar(49);
+				_putchar(1 + 48);
 				num = count % 10;
 
 			}
-			_putchar(48 + num);
+			_putchar(num + 48);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
