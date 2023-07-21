7c3761ebf9244dbda0d2dffbdd49094b72780332
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-21 11:09:09 -0700

    completed

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - A C function that draws a triangle
+ *
+ * @size: size of triangle
+*/
+
+void print_triangle(int size)
+{
+	int i = 0;
+	int k = 1, j = 0;
+
+	if (size > 0)
+	{
+		while (i <= size)
+		{
+			while (k <= size)
+			{
+				_putchar(' ');
+				k++;
+			}
+			while (j < i)
+			{
+				_putchar('#');
+				j++;
+			}
+			i++;
+			k = i + 1;
+			j = 0;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
