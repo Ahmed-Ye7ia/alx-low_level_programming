7ae693c57c37614d525b88da5d195ccc6ee7c9a5
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-24 15:13:51 -0700

    completed

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_array - A C function that prints an array elements
+ *
+ * @a: array parameter
+ *
+ * @n: size of array
+*/
+
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0 ; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			_putchar(a[i]);
+			i++;
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
