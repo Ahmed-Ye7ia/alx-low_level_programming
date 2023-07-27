329c2f9e127e78a39355b1c8393951160718e738
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-07-27 05:17:26 -0700

    completed

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncpy - A C funtion that copies a string
+ *
+ * @dest: the first string
+ *
+ * @src: the second string
+ *
+ * @n: the number of characters that will copy
+ *
+ * Return: it's return pointer to string after concatinate
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
