a1e41d04e1bf468233e5094f6f0454fbe9355c7d
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-05 13:36:07 -0700

    completed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * @argc: num of arguments
+ *
+ * @argv: array of arguments
+ *
+ * Return: always 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc);
+	return (0);
+}
