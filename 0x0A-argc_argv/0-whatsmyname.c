a772faa998b321ff558cd92e5600ce70b8989ed5
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-05 13:28:00 -0700

    completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * @argc: num of arguments
+ *
+ * @argv: array of arguments
+ *
+ * Return: always 0 
+*/
+
+int main(char const *argv[],int argc)
+{
+	printf ("%s \n", argv[0]);
+	return (0);
+}
