acf2c3e486e173e7aaaf7c6f33a4087f080242e1
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-06 06:11:40 -0700

    completed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,27 +13,20 @@
 
 int main(int argc, char *argv[])
 {
-	int sum = 0, i, num;
+	int sum = 0, i;
 
-	if (argc < 2)
+	for (i = 1 ; i < argc ; i++)
 	{
-		printf("%d\n", 0);
-	}
-	else
-	{
-		for (i = 1 ; i < argc ; i++)
+		if (*argv[i] >= 48 && *argv[i] <= 57)
 		{
-			num = (int)(argv[i]);
-			if (num >= 48 && num <= 57)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("%s\n", "Error");
-				return (1);
-			}
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("%s\n", "Error");
+			return (1);
 		}
 	}
+	printf("%d\n", sum);
 	return (0);
 }
