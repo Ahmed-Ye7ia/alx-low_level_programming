95a043b70c03dbfd620c08d4f8e92869e2f65daf
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-24 08:59:04 -0700

    completed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void pre_main(void) __attribute__((constructor));
+
+/**
+ * pre_main - functios that executes before main
+*/
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
