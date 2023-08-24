9c08bbef277d40c05c9ca5b095db3601e9179f68
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-24 07:42:47 -0700

    completed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ *_strlen - returns the length of a string
+ *@s: the string whose length to return
+ *Return: the length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
  *add_node - adds a new node at the beginning of list_t list
  *@head: address of node
  *@str: string to add
@@ -13,8 +28,12 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (!new_node || !head)
 		return (NULL);
-	new_node->str = strdup(str);
-	new_node->next = *head;
-	*head = new_node;
+	if (str)
+	{
+		new_node->str = strdup(str);
+		new_node->len = _strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+	}
 	return (new_node);
 }
