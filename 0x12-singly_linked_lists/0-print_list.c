0b3145cd4dd6cb38ac628e1e0e9b7ad61ec7d59d
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-24 06:36:06 -0700

    completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%i] %s\n", h->len ? h->len : 0, h->str ? h->str : "(nil)");
+		if (h)
+			printf("[%i] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		i++;
 	}
