02ffd2c0989cd9910032f7adb130ea25223ecf31
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-24 06:34:01 -0700

    completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,4 @@
 #include "lists.h"
-/**
- * _strlen - A C Function that return the length of string
- * @s: the String
- * Return: the length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
 
 /**
  * print_list - a function that prints all the elements of a list_t list.
@@ -26,7 +11,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
+		printf("[%i] %s\n", h->len ? h->len : 0, h->str ? h->str : "(nil)");
 		h = h->next;
 		i++;
 	}
