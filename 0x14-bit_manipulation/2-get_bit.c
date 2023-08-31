b46bb01a286c13ad6b209754fcf96ab10e73a615
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-08-31 07:46:01 -0700

    completed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int mask = 1;
-	unsigned int bit = sizeof(n) * 8 - 1;
+	unsigned int bit = sizeof(n) * 8;
 
 	if (index > bit)
 		return (-1);
