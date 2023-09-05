d95c01b510854e0bc9b00ac81026daa72301d4d8
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-09-05 06:27:32 -0700

    completed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,11 +10,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int op, wr, len = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
-	for (len = 0; text_content[len];)
-		len++;
+	if (text_content)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
