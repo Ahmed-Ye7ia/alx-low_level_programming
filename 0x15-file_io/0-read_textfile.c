8059302d769763926a785840ce35b4172aa0f30b
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-09-05 05:45:25 -0700

    completed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,6 @@
  *read_textfile - reads a text file and prints it to the POSIX standard output.
  *@filename: name of file to read
  *@letters: number of letters to read
- *
  *Return: the number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
